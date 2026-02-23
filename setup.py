9efeec6f88e18166cf74525a40c3fd274962e59a
Vladcom4iiik <solovyev4625@gmail.com>
2026-02-24 01:42:04 +0300

    Обновление версии на 1.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="netschoolpy",
-    version="1.0.2",
+    version="1.0.3",
     description="Асинхронный клиент для «Сетевого города»",
     long_description=long_description,
     long_description_content_type="text/markdown",
